c436ffdf2b2f0ddaee0fcaa37019131b8c43c6ec
zacce65 <sakari.heino@gmail.com>
2021-01-16 20:58:47 +0200

    fixes 16-1-21

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -4,11 +4,20 @@ var provider = new firebase.auth.GoogleAuthProvider();
 provider.addScope('profile');
 provider.addScope('email');
 
+export const login = (uid) => ({
+  type: 'LOGIN',
+  uid
+});
+
 export const startLogin = () => {
   return () => {
     return firebase.auth().signInWithPopup(provider);
   };
 };
+
+export const logout = () => ({
+  type: 'LOGOUT'
+});
 
 export const startLogout = () => {
   return () => {
